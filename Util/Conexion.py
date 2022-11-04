4cfa2f071c8d65ba9765902b0829e4a7cdd92e26
DesarrolloZK <soporteremoto@zonak.com.co>
2022-11-03 21:49:30 -0500

    Primeras configuraciones control conexion

--- Util/Conexion.py
+++ Util/Conexion.py
@@ -1,32 +1,59 @@
+from asyncio.windows_events import NULL
 import pyodbc
+#Clase para controlar la conexion con la base de datos
+class ControlConexion():
+    #Metodo constructor para asignar los datos necesarios para la conexion
+    def __init__(self,sName,db,user,password) -> None:
+        self.__serverName=sName
+        self.__dataBase=db
+        self.__sUser=user
+        self.__sPassword=password
+        self.__conexion=NULL
+        
+    #Funcion en cargada de realizar la conexion y retornar true si se establece la conexion
+    #y retornar flase si no se establece dicha conexion
+    def conectar(self)->bool:
+        try:            
+            self.__conexion=pyodbc.connect('DRIVER={ODBC Driver 18 for SQL server};'+
+            'SERVER='+self.__serverName+';DATABASE='+self.__dataBase+';UID='+self.__sUser+';PWD='+self.__sPassword+
+            ';ENCRYPT=No;')
+            return True
+        except Exception as e:
+            return False
 
-
-if __name__=='__main__':
+    #Funcion para realizar consultas en la base de datos
+    #Esta funcion  es solo de prueba.
+    def consulta(self,sentencia)->list:
+        try:
+            with self.__conexion.cursor() as cursor:
+                con=cursor.execute(sentencia+';').fetchall()
+                return con
+        except Exception as e:
+            print(f"Error al consultar: {e}")
     
-    try:
-        server="172.19.101.139\sqlexpress"
-        bd="DataStore"
-        username="ivkdb"
-        password="Grup0IVK1*"
-        conexion=pyodbc.connect('DRIVER={ODBC Driver 18 for SQL server};'+
-        'SERVER='+server+';DATABASE='+bd+';UID='+username+';PWD='+password+
-        ';ENCRYPT=No;')
-        print(f"Conexion a {server} exitosa\n")
-        
+    def getServerName(self)->str:
+        return self.__serverName
 
-    except Exception as e:
-        print(f"Error de conexion:\n {e}")
-
+    def getSUser(self)->str:
+        return self.__sUser
     
-    try:
-        with conexion.cursor() as cursor:
-            prueba=cursor.execute("select * from dbo.HFI_PAYLOAD;").fetchall()
-
-            for dat in prueba:
-                print(dat)
-        
-
-        
-
-    except Exception as e:
-        print(f"Error al consultar: {e}")
+    def getDataBase(self)->str:
+        return self.__dataBase
+           
+if __name__=="__main__":
+    listaConexiones=list()
+    test=ControlConexion("172.19.101.139\sqlexpress","DataStore","ivkdb","Grup0IVK1*")
+    if test.conectar():
+        print(f"Conexion establecida con {test.getServerName()}")
+        listaConexiones.append(test)
+    else:
+        print(f"Error al conectar con {test.getServerName()}")
+    test=ControlConexion("172.19.101.121\sqlexpress","DataStore","ivkdb","Grup0IVK1*")
+    if test.conectar():
+        print(f"Conexion establecida con {test.getServerName()}")
+        listaConexiones.append(test)
+    else:
+        print(f"Error al conectar con {test.getServerName()}")
+    #print(listaConexiones[0].consulta("select * from dbo.HFI_PAYLOAD"))
+    #print("---------------separardor----------------\n")
+    #print(listaConexiones[1].consulta("select * from dbo.MENU_ITEM_CLASS"))
